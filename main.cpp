93d35f577fd397cbb99346a513a612c033dad3d5
Nadezhda <nadezhdadieslast@gmail.com>
2024-06-16 18:27:13 +0400

    removed unnecessary array check

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 std::vector<int> moduloSort(std::vector<int> &vector) {
     for (int k = 0; k < vector.size() - 1; k++) {
         for (int i = 0; i < vector.size() - k - 1; i++) {
-            if ((abs(vector[i]) > abs(vector[i + 1])) && (i + 1 < vector.size())) {
+            if ((abs(vector[i]) > abs(vector[i + 1]))) {
                 std::swap(vector[i], vector[i + 1]);
             }
         }
